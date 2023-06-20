e3b4e0fa0dba02dbaddb1a6d48f6d4cbde910559
Brayan Silva <brayan.silva@sofka.com.co>
2023-06-19 21:20:46 -0500

    feat: change application name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,17 @@ const Header = () => {
     <>
       <div className="header">
         <div className="general-name">
-          <h1>BUSCBIAN</h1>
+          <h1>SIBUC</h1>
         </div>
         <div className="general-description">
           <p>
             SISTEMA DE <b>BÚSQUEDA</b>
           </p>
           <p>
-            DE <b>CATÁLOGOS</b> EN <b>BIBLIOTECAS</b>
+            DE <b>CATÁLOGOS</b> EN LA MESA 
           </p>
           <p>
-            DE <b>ANTIOQUIA</b>
+          DE <b>BIBLIOTECAS</b> DE <b>ANTIOQUIA</b>
           </p>
         </div>
         <img className="logo" src="/mesa-logo.png" alt="Logo" />
